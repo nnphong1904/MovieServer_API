7cecdefad80332dfb4f675ce72fff1e2f5367ea3
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-10-27 23:44:25 +0700

    add res.header to solve cors

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,5 +1,6 @@
 
 module.exports.postlogIn=function(req, res, next){   
     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.redirect('http://localhost:3001/movies');
 }
